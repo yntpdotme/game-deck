d243e06d66d47fc5483781999bd572758d75bc31
Akash Kadlag <akashkadlag14@gmail.com>
2024-05-24 16:27:54 +0530

    feat: display genres

--- client/src/components/GenreList.tsx
+++ client/src/components/GenreList.tsx
@@ -1,5 +1,7 @@
+import {Button} from '@/components/ui/button';
 import useGenres from '@/hooks/useGenres';
-import {Heading, List} from '@chakra-ui/react';
+import getCroppedImagedUrl from '@/services/imageUrl';
+import {Heading, HStack, Image, List} from '@chakra-ui/react';
 
 const GenreList = () => {
 	const {data: genres, error, isLoading} = useGenres();
@@ -9,13 +11,29 @@ const GenreList = () => {
 
 	return (
 		<>
-			<Heading fontSize="2xl" mb={3}>
+			<Heading fontSize="2xl" mt={4} mb={2}>
 				Genres
 			</Heading>
 			<List.Root>
 				{genres?.results.map(genre => (
-					<List.Item key={genre.id} py={1.5} >
-						{genre.name}
+					<List.Item key={genre.id} py={1} listStyle="none" ml={0.5}>
+						<HStack>
+							<Image
+								height={7}
+								borderRadius={4}
+								objectFit="cover"
+								src={getCroppedImagedUrl(genre.image_background)}
+							/>
+							<Button
+								variant="plain"
+								px={1.5}
+								m={0}
+								fontSize="lg"
+								fontWeight="normal"
+							>
+								{genre.name}
+							</Button>
+						</HStack>
 					</List.Item>
 				))}
 			</List.Root>
--- client/src/entities/Genre.ts
+++ client/src/entities/Genre.ts
@@ -1,6 +1,7 @@
 type Genre = {
 	id: number;
 	name: string;
+	image_background: string;
 };
 
 export default Genre;
