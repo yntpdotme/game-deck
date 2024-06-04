570c9783468067b8910380d41a43510306ca00c2
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-05 00:13:58 +0530

    feat: build game trailer

---
+++ client/src/hooks/useTrailers.ts
@@ -0,0 +1,14 @@
+import Trailer from '@/entities/Trailer';
+import APIClient from '@/services/apiClient';
+import {useQuery} from '@tanstack/react-query';
+
+const useTrailers = (gameId: number) => {
+	const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
+	return useQuery({
+		queryKey: ['trailers', gameId],
+		queryFn: apiClient.getAll,
+	});
+};
+
+export default useTrailers;
--- client/src/pages/GameDetailPage.tsx
+++ client/src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
 import ExpandableText from '@/components/ExpandableText';
 import GameAttributes from '@/components/GameAttributes';
+import GameTrailer from '@/components/GameTrailer';
 import useGame from '@/hooks/useGame';
-import {Box, Heading} from '@chakra-ui/react';
+import {GridItem, Heading, SimpleGrid} from '@chakra-ui/react';
 import {useNavigate, useParams} from 'react-router-dom';
 
 const GameDetailPage = () => {
@@ -14,13 +15,23 @@ const GameDetailPage = () => {
 	if (error || !game) navigate('/not-found');
 
 	return (
-		<Box padding="10px" mt={5}>
-			<Heading mb={6} fontSize="3xl">
-				{game.name}
-			</Heading>
-			<ExpandableText>{game.description_raw}</ExpandableText>
-			<GameAttributes game={game} />
-		</Box>
+		<SimpleGrid
+			padding="10px"
+			mt={5}
+			columns={{base: 1, lg: 2}}
+			gap={{base: 6, lg: 8}}
+		>
+			<GridItem>
+				<Heading mb={6} fontSize="3xl">
+					{game.name}
+				</Heading>
+				<ExpandableText>{game.description_raw}</ExpandableText>
+				<GameAttributes game={game} />
+			</GridItem>
+			<GridItem mt={2}>
+				<GameTrailer gameId={game.id} />
+			</GridItem>
+		</SimpleGrid>
 	);
 };
 
